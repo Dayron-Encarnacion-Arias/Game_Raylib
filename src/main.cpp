26ec23cc8caf8ba7c1070b8d59f67433f233c0f4
Pica-Pollo <dayronearias@gmail.com>
2026-08-26 12:32:54 -0400

    First Program
    i created a Hello, Raylib!

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,17 @@
 
 int main() 
 {
+      InitWindow(800, 600, "Hello Raylib");
+      SetTargetFPS(60);
+   
+      while (!WindowShouldClose()) 
+      {
+         BeginDrawing();
+         ClearBackground(RAYWHITE);
+         DrawText("Hello, Raylib!", 190, 200, 20, LIGHTGRAY);
+         EndDrawing();
+      }
+   
+      CloseWindow();
    return 0;
 }
